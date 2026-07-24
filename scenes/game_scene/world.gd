1849d41aad092e967a3978e62a35f931bae9a84a
Jean Schmitt <github@jeanschmitt.fr>
2026-07-24 19:16:16 +0200

    feat: update world scene with generation params

--- scenes/game_scene/world.gd
+++ scenes/game_scene/world.gd
@@ -6,8 +6,3 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	level_generator.instantiate_level(self, first_connector, randi())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
