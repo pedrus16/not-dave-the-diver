89080b29f097c2f90e3282d9b37a4959d5d46965
Jean Schmitt <github@jeanschmitt.fr>
2026-07-24 10:49:08 +0200

    feat: spawn modules under poulette

--- dev_scenes/level_generation/level_generation.gd
+++ dev_scenes/level_generation/level_generation.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @export var level_generator: LevelGenerator
 @export var first_connector: ModuleConnector
+@export var modules_root: Node2D
 
 
 func _ready() -> void:
-	level_generator.instantiate_level(self, first_connector, randi())
+	level_generator.instantiate_level(modules_root, first_connector, randi())
