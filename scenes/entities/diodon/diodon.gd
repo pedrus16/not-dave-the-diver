1dcf82ff233a150db580ca7ea30d443997243a7c
pedrus <pierre.nole@gmail.com>
2026-07-26 00:06:25 +0200

    feat: make diodon floats after inflating

--- scenes/entities/diodon/diodon.gd
+++ scenes/entities/diodon/diodon.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-var _inflated := false
+@export var inflated := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,12 +13,14 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var ticks = Time.get_ticks_msec()
-	constant_force = Vector2(0, cos(ticks * 0.001)) * 100.0
+	if not inflated:
+		constant_force = Vector2(0, cos(ticks * 0.001)) * 100.0
+	else:
+		constant_force = Vector2(0, -200.0)
 
 
 func _on_aggro_area_body_entered(body: Node2D) -> void:
-	if _inflated: return
+	if inflated: return
 	var character = BaseEffect.find_character(body)
 	if character:
 		%AnimationPlayer.play("blow")
-		_inflated = true
