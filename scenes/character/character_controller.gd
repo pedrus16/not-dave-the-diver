424c730e9034183ac05b117c34b8523504a6a3ff
pedrus <pierre.nole@gmail.com>
2026-07-25 12:25:48 +0200

    chore: refactor buoyancy

--- scenes/character/character_controller.gd
+++ scenes/character/character_controller.gd
@@ -5,7 +5,6 @@ class_name CharacterController extends Node2D
 @export var inventory: CharacterInventory
 @export var o2_counter: RefillableTimer
 @export var movePower := 240.0
-@export var buoyancy := 10.0
 @export var refill_rate := 2.0
 
 @export var disable_controls := false
@@ -13,7 +12,7 @@ class_name CharacterController extends Node2D
 var sprite_rotation := 0.0
 var is_swimming := false
 var in_water := false
-var _last_input_angle := 0.0
+var _last_input := Vector2.ZERO
 var _dir_changed_elapsed_time := 0.0
 var _sprite_angle_offset := PI * 0.5
 var _dead := false
@@ -22,32 +21,30 @@ var _dead := false
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _unhandled_input(event: InputEvent) -> void:
+	if disable_controls == true: return
+	
+	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_delta := input - _last_input
+	rigidBody2D.add_constant_force(input_delta * movePower)
+	
+	if not Vector2(_last_input - input).is_zero_approx():
+		_dir_changed_elapsed_time = 0.0
+		_last_input = input
+		
+	is_swimming = not input.is_zero_approx()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if disable_controls == true: return
-	
-	var input := Vector2.ZERO
-	if not disable_controls:
-		input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
-	rigidBody2D.constant_force = input * movePower + Vector2(0, -buoyancy)
-	
-	is_swimming = not input.is_zero_approx()
-	var input_angle = input.angle()
-	
-	if input.is_zero_approx(): return
-	
 	_dir_changed_elapsed_time += delta * Engine.time_scale
 	
-	if abs(sprite.rotation - (_last_input_angle + _sprite_angle_offset)) > 0.001:
-		sprite_rotation = lerp_angle(sprite.rotation, _last_input_angle + _sprite_angle_offset, min(1.0, _dir_changed_elapsed_time))
+	if _last_input.is_zero_approx(): return
+	
+	var last_input_angle = _last_input.angle()
+	if abs(sprite.rotation - (last_input_angle + _sprite_angle_offset)) > 0.001:
+		sprite_rotation = lerp_angle(sprite.rotation, last_input_angle + _sprite_angle_offset, min(1.0, _dir_changed_elapsed_time))
 	else:
-		sprite_rotation = _last_input_angle + _sprite_angle_offset
-
-	if _last_input_angle != input_angle:
-		_dir_changed_elapsed_time = 0.0
-		_last_input_angle = input_angle
+		sprite_rotation = last_input_angle + _sprite_angle_offset
 
 
 func _process(delta: float) -> void:
@@ -57,6 +54,7 @@ func _process(delta: float) -> void:
 	if is_swimming:
 		sprite.animation = "swimming"
 	else:
+		sprite.animation
 		sprite.animation = "idle"
 		
 	if not in_water:
@@ -67,14 +65,14 @@ func _on_breath_area_area_entered(area: Area2D) -> void:
 	if not area.is_in_group("air"): return
 	
 	in_water = false
-	rigidBody2D.gravity_scale = 1.0
+	#rigidBody2D.gravity_scale = 1.0
 
 
 func _on_breath_area_area_exited(area: Area2D) -> void:
 	if not area.is_in_group("air"): return
 	
 	in_water = true
-	rigidBody2D.gravity_scale = 0.0
+	#rigidBody2D.gravity_scale = 0.0
 
 
 func _on_o_2_counter_timeout() -> void:
@@ -95,4 +93,3 @@ func kill() -> void:
 	_dead = true
 	sprite.animation = "dying"
 	disable_controls = true
-	rigidBody2D.constant_force = Vector2(0, -buoyancy)
