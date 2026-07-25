49a8060dc6f92ae4b9ff1fb127d7bc15dba556ba
pedrus <pierre.nole@gmail.com>
2026-07-25 10:59:04 +0200

    feat: add death

--- scenes/character/character_controller.gd
+++ scenes/character/character_controller.gd
@@ -4,18 +4,19 @@ class_name CharacterController extends Node2D
 @export var sprite: AnimatedSprite2D
 @export var inventory: CharacterInventory
 @export var o2_counter: RefillableTimer
-@export var movePower: float = 240.0
-@export var buoyancy: float = 10.0
-@export var refill_rate: float = 2.0
+@export var movePower := 240.0
+@export var buoyancy := 10.0
+@export var refill_rate := 2.0
 
-@export var disable_controls: bool = false
+@export var disable_controls := false
 
-var sprite_rotation: float = 0.0
-var is_swimming: bool = false
-var in_water: bool = false
-var _last_input_angle: float = 0.0
-var _dir_changed_elapsed_time: float = 0.0
-var _sprite_angle_offset = PI * 0.5
+var sprite_rotation := 0.0
+var is_swimming := false
+var in_water := false
+var _last_input_angle := 0.0
+var _dir_changed_elapsed_time := 0.0
+var _sprite_angle_offset := PI * 0.5
+var _dead := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,11 +27,15 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if disable_controls == true: return
 	
-	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input := Vector2.ZERO
+	if not disable_controls:
+		input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
 	rigidBody2D.constant_force = input * movePower + Vector2(0, -buoyancy)
 	
 	is_swimming = not input.is_zero_approx()
 	var input_angle = input.angle()
+	
 	if input.is_zero_approx(): return
 	
 	_dir_changed_elapsed_time += delta * Engine.time_scale
@@ -46,6 +51,8 @@ func _physics_process(delta: float) -> void:
 
 
 func _process(delta: float) -> void:
+	if _dead: return
+	
 	sprite.rotation = sprite_rotation
 	if is_swimming:
 		sprite.animation = "swimming"
@@ -54,16 +61,6 @@ func _process(delta: float) -> void:
 		
 	if not in_water:
 		o2_counter.add_time(refill_rate * delta)
-		
-
-
-## Adds a positive or negative number of seconds to o2 timer.
-func add_o2_delta(seconds: float) -> void:
-	o2_counter.add_time(seconds)
-
-
-func take_item(item: Node2D) -> void:
-	inventory.take_item(item)
 
 
 func _on_breath_area_area_entered(area: Area2D) -> void:
@@ -78,3 +75,24 @@ func _on_breath_area_area_exited(area: Area2D) -> void:
 	
 	in_water = true
 	rigidBody2D.gravity_scale = 0.0
+
+
+func _on_o_2_counter_timeout() -> void:
+	kill()
+
+
+## Adds a positive or negative number of seconds to o2 timer.
+func add_o2_delta(seconds: float) -> void:
+	o2_counter.add_time(seconds)
+
+
+func take_item(item: Node2D) -> void:
+	inventory.take_item(item)
+
+
+## Triggers death animation and disable controls
+func kill() -> void:
+	_dead = true
+	sprite.animation = "dying"
+	disable_controls = true
+	rigidBody2D.constant_force = Vector2(0, -buoyancy)
