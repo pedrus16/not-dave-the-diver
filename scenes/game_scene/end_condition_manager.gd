45b175973aa8c02721760ed384a9e075715e8758
Jean Schmitt <github@jeanschmitt.fr>
2026-07-26 00:31:11 +0200

    feat: add victory overlay

---
+++ scenes/game_scene/end_condition_manager.gd
@@ -0,0 +1,48 @@
+extends Node
+
+signal victory
+signal defeat
+
+@export var character: CharacterController
+@export var surface_node: Node2D
+@export var items_root: Node
+@export var egg_scene: PackedScene
+@export var bubble_counter: Control
+
+var _ended := false
+
+
+func _process(_delta: float) -> void:
+	if _ended:
+		return
+	
+	if character.rigidBody2D.global_position.y < surface_node.global_position.y:
+		for child in items_root.get_children():
+			if child.scene_file_path == egg_scene.resource_path:
+				_play_victory()
+
+
+func _play_victory() -> void:
+	if _ended:
+		return
+
+	_hide_bubble_counter()
+	
+	_ended = true
+	victory.emit()
+
+
+func _on_character_died() -> void:
+	if _ended:
+		return
+
+	_ended = true
+	
+	await character.sprite.animation_finished
+	
+	_hide_bubble_counter()
+	defeat.emit()
+
+
+func _hide_bubble_counter() -> void:
+	create_tween().tween_property(bubble_counter, "alpha", 0.0, 1.0)
--- scenes/game_scene/overlay_manager.gd
+++ scenes/game_scene/overlay_manager.gd
@@ -1,14 +1,30 @@
 extends Node
 
+@export var victory_overlay: Control
 @export var defeat_overlay: Control
 
 
 func _ready() -> void:
-	defeat_overlay.visible = false
+	_hide_all_overlays()
+
+
+func show_victory() -> void:
+	_show_overlay(victory_overlay)
 
 
 func show_defeat() -> void:
-	defeat_overlay.modulate.a = 0.0
-	defeat_overlay.visible = true
+	_show_overlay(defeat_overlay)
+
+
+func _show_overlay(overlay: Control) -> void:
+	_hide_all_overlays()
 	
-	create_tween().tween_property(defeat_overlay, "modulate:a", 1.0, 1.0)
+	overlay.modulate.a = 0.0
+	overlay.visible = true
+	
+	create_tween().tween_property(overlay, "modulate:a", 1.0, 1.0)
+
+
+func _hide_all_overlays() -> void:
+	victory_overlay.visible = false
+	defeat_overlay.visible = false
