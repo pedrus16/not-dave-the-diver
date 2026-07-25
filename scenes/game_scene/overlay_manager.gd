c2b677f09253a7d73bd53db378920549a0e69629
Jean Schmitt <github@jeanschmitt.fr>
2026-07-26 00:44:03 +0200

    feat: pause overlay

--- scenes/game_scene/overlay_manager.gd
+++ scenes/game_scene/overlay_manager.gd
@@ -2,10 +2,19 @@ extends Node
 
 @export var victory_overlay: Control
 @export var defeat_overlay: Control
+@export var pause_overlay: Control
 
 
 func _ready() -> void:
 	_hide_all_overlays()
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed(&"pause"):
+		if pause_overlay.visible:
+			exit_pause()
+		elif !victory_overlay.visible && !defeat_overlay.visible:
+			show_pause()
 
 
 func show_victory() -> void:
@@ -14,6 +23,18 @@ func show_victory() -> void:
 
 func show_defeat() -> void:
 	_show_overlay(defeat_overlay)
+
+
+func show_pause() -> void:
+	_hide_all_overlays()
+	
+	get_tree().paused = true
+	pause_overlay.visible = true
+
+
+func exit_pause() -> void:
+	pause_overlay.visible = false
+	get_tree().paused = false
 
 
 func _show_overlay(overlay: Control) -> void:
@@ -28,3 +49,6 @@ func _show_overlay(overlay: Control) -> void:
 func _hide_all_overlays() -> void:
 	victory_overlay.visible = false
 	defeat_overlay.visible = false
+	pause_overlay.visible = false
+	
+	get_tree().paused = false
