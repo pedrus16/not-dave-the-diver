c4134ed19ad74e846003c3dd9980bbd75bd92698
Jean Schmitt <github@jeanschmitt.fr>
2026-07-26 16:52:51 +0200

    feat: pre-play sounds

--- game.gd
+++ game.gd
@@ -5,6 +5,7 @@ extends Node
 
 var _world_instance: Node2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,13 +19,50 @@ func _process(delta: float) -> void:
 func _on_menu_start() -> void:
 	_start_game()
 
+
 func _start_game() -> void:
 	_world_instance = world.instantiate()
 	_world_instance.new_game_requested.connect(_back_to_menu)
 	_world_instance.main_menu_requested.connect(_back_to_menu)
 	%World2D.add_child(_world_instance)
+	
+	await _preload_audio_streams(_world_instance)
+	
 	%Menu.hide()
+
 
 func _back_to_menu() -> void:
 	%World2D.remove_child(_world_instance)
 	%Menu.show()
+
+
+func _preload_audio_streams(game: Node2D) -> void:
+	var canvas_layers := game.find_children("", "CanvasLayer")
+	var buses: Dictionary[AudioStreamPlayer, StringName] = {}
+	
+	game.visible = false
+	for layer in canvas_layers:
+		layer.visible = false
+	
+	game.set_process(PROCESS_MODE_DISABLED)
+	
+	for child in game.find_children("", "AudioStreamPlayer"):
+		var player := child as AudioStreamPlayer
+		buses[player] = player.bus
+		player.bus = &"MuteBus"
+		player.play()
+	
+	await get_tree().create_timer(0.2).timeout
+	
+	for player in buses:
+		player.stop()
+		player.seek(0.0)
+		player.bus = buses[player]
+	
+	game.set_process(PROCESS_MODE_INHERIT)
+	
+	game.visible = true
+	for layer in canvas_layers:
+		layer.visible = true
+	
+	
