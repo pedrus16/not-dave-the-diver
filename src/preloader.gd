044341561b5d8f9b94629c92065bfe7a54edc91a
Jean Schmitt <github@jeanschmitt.fr>
2026-07-26 17:07:19 +0200

    feat: preload bubble shader

--- src/preloader.gd
+++ src/preloader.gd
@@ -4,12 +4,15 @@ class_name Preloader extends Node
 signal loaded
 
 @export var audio_folder := "res://audio"
+@export var preload_shaders: Array[Shader]
 @export var game_scene: PackedScene
 
 var _mute_audio_player := AudioStreamPlayer.new()
 var _hidden_node_2d := Node2D.new()
+var _hidden_sprite := Sprite2D.new()
 
 var _audio_paths_to_load: Array[String] = []
+var _shaders_to_load: Array[Shader]
 var _loaded: Dictionary[String, Variant]
 
 var _rope_lib_loaded := false
@@ -20,9 +23,14 @@ var _busy := false
 func _ready() -> void:
 	_audio_paths_to_load.append_array(_list_audio_files(audio_folder))
 	
+	_shaders_to_load = preload_shaders.duplicate()
+	
 	_hidden_node_2d.visible = false
 	_hidden_node_2d.global_position = Vector2(100000, 100000)
 	add_child(_hidden_node_2d)
+	
+	_hidden_sprite.global_position = Vector2(100000, 100000)
+	add_child(_hidden_sprite)
 	
 	_mute_audio_player.volume_db = -80
 	add_child(_mute_audio_player)
@@ -37,6 +45,10 @@ func _process(_delta: float) -> void:
 		_load_rope_lib()
 		return
 	
+	if !_shaders_to_load.is_empty():
+		_load_shaders()
+		return
+	
 	if !_hidden_game_loaded:
 		_spawn_hidden_game()
 		return
@@ -45,6 +57,7 @@ func _process(_delta: float) -> void:
 	print("Loaded all resources")
 	set_process(false)
 	_hidden_node_2d.queue_free()
+	_hidden_sprite.queue_free()
 	_mute_audio_player.queue_free()
 
 
@@ -77,6 +90,20 @@ func _load_rope_lib() -> void:
 	rope.queue_free()
 	
 	_rope_lib_loaded = true
+	_busy = false
+
+
+func _load_shaders() -> void:
+	if _busy:
+		return
+	
+	_busy = true
+	
+	var shader := _shaders_to_load.pop_back() as Shader
+	var mat := ShaderMaterial.new()
+	mat.shader = shader
+	_hidden_sprite.material = mat
+	
 	_busy = false
 
 
