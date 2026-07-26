54db7b234ef6c3e082254ff371395a92d9af1e9f
Jean Schmitt <github@jeanschmitt.fr>
2026-07-26 10:41:07 +0200

    feat: configure rope from the editor

--- scenes/character/life_rope_controller.gd
+++ scenes/character/life_rope_controller.gd
@@ -2,9 +2,9 @@ class_name LifeRopeController extends Node
 
 @export var debug_print_points_count := false
 
+@export var rope: CRope2D
 @export var character_body: RigidBody2D
 @export var surface_anchor: Node2D
-@export var rope_parent: Node2D
 
 @export_group("Rope behavior")
 @export_range(100, 2000) var max_points := 600
@@ -13,14 +13,6 @@ class_name LifeRopeController extends Node
 @export_range(1.05, 5.0) var grow_ratio := 1.1
 @export_range(1.0, 100.0) var retract_speed := 10.0
 @export_range(0.0, 100000.0, 1.0, "exp") var retract_pull_strength := 3000.0
-@export_flags_2d_physics var collision_mask: int
-
-@export_group("Rope render")
-@export var color: Color = Color.BROWN
-@export var shader: Shader = preload("res://scenes/character/rope.gdshader")
-@export_range(1.0, 30.0) var width := 10.0
-@export_range(0.01, 1.0) var stripe_width := 0.5
-@export_range(0.0, 0.5) var outline_width := 0.2
 
 var retracting := false:
 	set(val):
@@ -28,7 +20,6 @@ var retracting := false:
 			retracting = val
 			_on_retracting_changed()
 
-var _rope: CRope2D
 var _character_anchor: CRopeAnchor
 var _surface_anchor: CRopeAnchor
 var _is_character_attached: bool
@@ -36,7 +27,7 @@ var _retract_timer: float
 
 
 func _physics_process(delta: float) -> void:
-	if _rope == null || !_is_character_attached:
+	if !_is_character_attached:
 		return
 	
 	if retracting:
@@ -46,18 +37,10 @@ func _physics_process(delta: float) -> void:
 
 
 func create() -> void:
-	if _rope != null:
-		push_error("Rope already created")
-		return
-
 	var data := CRopeData.new()
 	data.create_line_by_length(character_body.global_position, surface_anchor.global_position, segments_length)
 	
-	_rope = CRope2D.new()
-	_rope.damping = 1.0 # avoids the rope to go up very fast when detaching it form the character
-	_rope.collision_stride = 1 # avoids glitches with colliders
-	_rope.collision_mask = collision_mask
-	_rope.data = data
+	rope.data = data
 
 	# Surface anchor has last index, because it is more performant to append points to the end
 	
@@ -73,33 +56,8 @@ func create() -> void:
 	_surface_anchor.collision_resolve = false
 	_surface_anchor.pull_strength = 0.0
 
-	_rope.anchors = [_surface_anchor, _character_anchor]
+	rope.anchors = [_surface_anchor, _character_anchor]
 	_is_character_attached = true
-
-	# Modules
-	_rope.force_modules = [CRopeGravityForceMod.new()]
-	
-	var smooth = CRopeSmoothLineMod.new()
-	var simplify = CRopeSimplifyLineMod.new()
-	_rope.line_modules = [smooth, simplify]
-	
-	var mat := ShaderMaterial.new()
-	mat.shader = shader
-	mat.set_shader_parameter("rope_color1", color.darkened(0.7))
-	mat.set_shader_parameter("rope_color2", color)
-	mat.set_shader_parameter("stripe_width", stripe_width)
-	mat.set_shader_parameter("outline_width", outline_width)
-	_rope.material = mat
-	
-	var renderer := CRopeDirectRenderMod.new()
-	renderer.width = width
-	renderer.begin_cap_mode = Line2D.LINE_CAP_ROUND
-	renderer.end_cap_mode = Line2D.LINE_CAP_ROUND
-	renderer.joint_mode = Line2D.LINE_JOINT_SHARP
-	
-	_rope.render_modules = [renderer]
-
-	rope_parent.add_child(_rope)
 
 
 func is_character_attached() -> bool:
@@ -107,7 +65,7 @@ func is_character_attached() -> bool:
 
 
 func detach_character() -> void:
-	_rope.anchors = [_surface_anchor]
+	rope.anchors = [_surface_anchor]
 	_is_character_attached = false
 
 
@@ -123,7 +81,7 @@ func try_attach_character() -> bool:
 		return false
 	
 	_character_anchor.index = point_dict["index"]
-	_rope.anchors = [_surface_anchor, _character_anchor]
+	rope.anchors = [_surface_anchor, _character_anchor]
 	
 	_is_character_attached = true
 	
@@ -131,7 +89,7 @@ func try_attach_character() -> bool:
 
 
 func _closest_point(from: Vector2) -> Dictionary:
-	var points := _rope.data.get_global_points()
+	var points := rope.data.get_global_points()
 	
 	var closest_index := -1
 	var closest_dist := INF
@@ -153,18 +111,18 @@ func _closest_point(from: Vector2) -> Dictionary:
 func _process_retractation(delta: float) -> void:
 	_retract_timer += delta
 	
-	if _retract_timer < 1.0 / retract_speed || _rope.data.get_count() <= 2:
+	if _retract_timer < 1.0 / retract_speed || rope.data.get_count() <= 2:
 		return
 	
 	_retract_timer = 0.0
 	
-	_rope.data.remove()
+	rope.data.remove()
 	_update_surface_anchor_index()
 
 
 func _process_expansion() -> void:
 	# We grow the rope if avg of 2 last segments is > than a threshold
-	var data := _rope.data
+	var data := rope.data
 	var pts := data.points
 	var last := data.get_count() - 1
 	var avg_segment := 0.5 * (pts[last].distance_to(pts[last-1]) + pts[last-1].distance_to(pts[last-2]))
@@ -199,7 +157,7 @@ func _on_retracting_changed() -> void:
 
 
 func _update_surface_anchor_index() -> void:
-	var count := _rope.data.get_count()
+	var count := rope.data.get_count()
 	_surface_anchor.index = count - 1
 	
 	if debug_print_points_count:
